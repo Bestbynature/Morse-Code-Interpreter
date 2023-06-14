fb9b02a1a7f25039089d090ce76ba934047ae975
Damilare <bestbynature2010@gmail.com>
2023-06-14 10:06:22 +0100

    :hammer: Fix linter errors

--- charDecoder.rb
+++ charDecoder.rb
@@ -1,48 +1,48 @@
 def decode_char(morse_code)
   lexicon = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
   }
 
-  return lexicon[morse_code]
+  lexicon[morse_code]
 end
 
 def decode_word(morse_word)
-  characters = morse_word.split(" ")
+  characters = morse_word.split(' ')
   decoded_word = characters.map { |morse_char| decode_char(morse_char) }
-  return decoded_word.join
+  decoded_word.join
 end
 
 def decode(morse_message)
-  words = morse_message.split("   ")
+  words = morse_message.split('   ')
   decoded_message = words.map { |word| decode_word(word) }
-  return decoded_message.join(' ')
+  decoded_message.join(' ')
 end
 
-oldBottle = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-decoded_message = decode(oldBottle)
+old_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decode(old_bottle)
 print "Decoded message: #{decoded_message}"
