e37c51ab0962b27c99aed391e75f375da1a87951
prius2055 <prius2055@gmail.com>
2023-06-14 09:48:37 +0100

    Update message decoder

--- charDecoder.rb
+++ charDecoder.rb
@@ -31,8 +31,19 @@ def decode_char(morse_code)
   return lexicon[morse_code]
 end
 
+
 def decode_word(morse_word)
   characters = morse_word.split(" ")
   decoded_word = characters.map { |morse_char| decode_char(morse_char) }
   return decoded_word.join
 end
+
+def decode(morse_message)
+  words = morse_message.split("   ")
+  decoded_message = words.map { |word| decode_word(word) }
+  return decoded_message.join
+end
+
+oldBottle = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+decoded_message = decode(oldBottle)
+print "Decoded message: #{decoded_message}"
