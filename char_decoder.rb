79a753d89ea36300941558d553f3d8d4fe05b7ac
Damilare <bestbynature2010@gmail.com>
2023-06-23 10:26:45 +0100

    :zap: Introduce a bug to be deciphered with pry gem

--- char_decoder.rb
+++ char_decoder.rb
@@ -12,7 +12,7 @@ def decode_char(morse_code)
   @lexicon[morse_code]
 end
 
-def decode_word(morse_word)
+def decode_word(morse_words)
   characters = morse_word.split
   decoded_word = characters.map { |morse_char| decode_char(morse_char) }
   decoded_word.join
@@ -20,10 +20,10 @@ end
 
 def decode(morse_message)
   words = morse_message.split('   ')
-  decoded_message = words.map { |word| decode_word(word) }
+  decoded_message = words.map { |words| decode_word(words) }
   decoded_message.join(' ')
 end
 
 old_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_message = decode(old_bottle)
-print "Decoded message: #{decoded_message}"
+puts "Decoded message: #{decoded_message}"
